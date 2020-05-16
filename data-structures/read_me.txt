d14701482c06b43c5f8f4a9ba34d65e90a3f817d
Shane Moe <shanekhantsoemoe98@gmail.com>
2020-05-16 15:59:27 -0400

    :sparkles: trie init

--- data-structures/read_me.txt
+++ data-structures/read_me.txt
@@ -1,3 +1,3 @@
 This directory contains the implementations of data structures in python.
 Implemented Data Structures:
-1.
+1. Trie
--- learning/tips-tricks.py
+++ learning/tips-tricks.py
@@ -40,7 +40,38 @@ import sys
 x = 1000000
 print(sys.getsizeof(x), "bytes")
 
-#
+# print string n times
+n = 10
+print("test " * n)
+
+# checking anagram
+from collections import Counter
+print(Counter("test") == Counter("tset"))
+
+# using ternary operator
+class A:
+    def p(self):
+        print("A")
+
+class B:
+    def p(self):
+        print("B")
+
+ternary = 100 if 10 == 10 else 90
+
+# ternary operator can be used for objects too
+a = A()
+b = B()
+(a if ternary == 90 else b).p()
+
+# multiline print
+multiline_str = """I am Shane.
+Nice to meet you!
+"""
+print(multiline_str)
+
+a,b,c = [1,2,3]
+print(a,b,c)
 
 
 
